dd1e683b522303c622cf99c7e32f4ea3d1aeb634
Vince <vinliu356@gmail.com>
2024-03-29 02:14:46 -0700

    again

--- ecce-app/vite.config.js
+++ ecce-app/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+  base: './',
   plugins: [react()],
 })
